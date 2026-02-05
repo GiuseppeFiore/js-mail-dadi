6ca3cfbf747d2fa725e091b2540dc5d38922b513
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 13:40:37 +0100

    created variables mail and prompt

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,6 +1,22 @@
+// Mail
 // Crea una lista di email di invitati ad una festa.
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
 // Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
 // Si può fare? Certo che si basta ragionare un po’.
+
+
+// Inizio con la creazione delle lista email di invitati alla festa
+const listaEmail = [
+    "mario@gmail.com",
+    "luigi@gmail.com",
+    "giuseppe@gmail.com",
+    "andrea@gmail.com",
+    "giada@gmail.com"
+];
+
+// Uso il prompt per chiedere l'email all'utente
+let emailUtente = prompt("Inserisci la tua email");
+
+
