a142388b7767dfd104b4081217ec53a288d70581
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 14:38:02 +0100

    created a variables for dadi

--- assets/js/dadi.js
+++ assets/js/dadi.js
@@ -4,3 +4,10 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+// Genero un numero random da 1 a 6 per il giocatore utilizzando parseInt perchè vogliamo solo numeri interi senza decimali o altro
+// Con math.random genero un numero del tutto casuale in questo caso compreso da 1 a 6 
+const dadoGiocatore = parseInt (Math.random() * 6) + 1;
+const dadoComputer = parseInt (Math.random() * 6) + 1;
+
