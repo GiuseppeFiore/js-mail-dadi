84c11acaa31d33085914f196be3a93f4a2f6c8e8
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 14:18:30 +0100

    final commit mail

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -14,9 +14,27 @@ const listaEmail = [
     "giuseppe@gmail.com",
     "andrea@gmail.com",
     "giada@gmail.com"
-];
+]
 
 // Uso il prompt per chiedere l'email all'utente
 let emailUtente = prompt("Inserisci la tua email");
+// Creo una condizione if dove l'utente se soddisfa avra "true" come risposta ed entra nell'if e prosegue
+// di conseguenza questa se l'email non è stata trovata ed esce dall'if con "false"
+if (emailUtente !== null) {
+    let emailTrovata = false;
 
-
+// Creo un ciclo for con Array finchè i è minore della lunghezza dell'array 
+    for (let i = 0; i <= listaEmail.length; i++) {
+// Con questo ciclo prendo l'email dell'utente in posizione i nella lista e se sono uguali vuol dire che 
+// l'utente appartiene alla lista degli invitati
+        if (emailUtente === listaEmail[i]) {
+        emailTrovata = true;
+        }
+    }
+// Se corrisponde e si sono verificato tutto l'accesso viene consentito altrimenti stampa l'accesso negato
+    if (emailTrovata) {
+        console.log("Accesso consentito: Ciao!! Sei invitato alla festa 🥳 🎊 🎉 🎁");
+    } else {
+        console.log("Accesso negato: email non presente ❌, Non sei invitato alla festa 😢");
+    }
+}
